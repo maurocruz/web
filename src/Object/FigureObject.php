1be38bf1cfc10f1830db7df8bebf369a85d8f9c2
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2020-11-12 17:15:28 -0300

    fix error in href var if ''

--- src/Object/FigureObject.php
+++ src/Object/FigureObject.php
@@ -4,11 +4,14 @@ namespace Plinct\Web\Object;
 
 class FigureObject {
     
-    public function __invoke($value) {        
-        $attributes = $value['attributes'] ?? null;        
+    public function __invoke($value)
+    {
+        $attributes = $value['attributes'] ?? null;
+        $href = isset($value['href']) && $value['href'] != '' ? $value['href'] : null ;
+
         // title
         if (array_key_exists('title', $value) && $value['title'] !== "") {
-            $content[] = [ "tag" => "h1", "content" => $value['title'], "href" => $value['href'] ?? null ];
+            $content[] = [ "tag" => "h1", "content" => $value['title'], "href" => $href ];
         }
         // img
         if (array_key_exists('src', $value)) {
@@ -17,7 +20,8 @@ class FigureObject {
         }
         // caption
         if (array_key_exists('caption', $value) && $value['caption'] == true) {
-            $content[] = [ "tag" => "figcaption", "content" => $value['caption'], "href" => $value['href'] ?? null ];
+            //var_dump($value);
+            $content[] = [ "tag" => "figcaption", "attributes" => $value['figcaptionAttributes'] ?? null, "content" => $value['caption'], "href" => $href ];
         }
         // content
         if (array_key_exists('content', $value) && $value['content'] == true) {
