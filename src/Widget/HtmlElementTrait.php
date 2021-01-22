e88d24ed0d96ec5c9347873d68a8aeb29632939a
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-01-22 14:28:44 -0300

    included return types

--- src/Widget/HtmlElementTrait.php
+++ src/Widget/HtmlElementTrait.php
@@ -4,17 +4,17 @@ namespace Plinct\Web\Widget;
 
 trait HtmlElementTrait 
 {    
-    protected static function simpleTag(string $tag, $content = null, array $attributes = null) 
+    protected static function simpleTag(string $tag, $content = null, array $attributes = null): array
     {
         return [ "tag" => $tag, "attributes" => $attributes, "content" => $content ];
     } 
     
-    protected static function noContent() 
+    protected static function noContent(): array
     {
         return self::simpleTag("p", _("No content"), [ "class" => "warning"]);
     }
 
-    protected static function arrowBack()
+    protected static function arrowBack(): array
     {
         return [ "object" => "image", "src" => "/App/static/cms/images/arrowBack.svg", "attributes" => [ "style" => "height: 48px; margin: 15px;"], "href" => "javascript: history.back();" ];
     }
