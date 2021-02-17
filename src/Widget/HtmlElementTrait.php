e4361db4348db75184f95ecdc4cb056734954e1c
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-02-17 15:56:20 -0300

    Created function scriptFotJsonLd()

--- src/Widget/HtmlElementTrait.php
+++ src/Widget/HtmlElementTrait.php
@@ -2,20 +2,23 @@
 
 namespace Plinct\Web\Widget;
 
-trait HtmlElementTrait 
+use Plinct\Tool\StructuredData;
+
+trait HtmlElementTrait
 {    
-    protected static function simpleTag(string $tag, $content = null, array $attributes = null): array
-    {
+    protected static function simpleTag(string $tag, $content = null, array $attributes = null): array {
         return [ "tag" => $tag, "attributes" => $attributes, "content" => $content ];
     } 
     
-    protected static function noContent(): array
-    {
+    protected static function noContent(): array {
         return self::simpleTag("p", _("No content"), [ "class" => "warning"]);
     }
 
-    protected static function arrowBack(): array
-    {
+    protected static function arrowBack(): array {
         return [ "object" => "image", "src" => "/App/static/cms/images/arrowBack.svg", "attributes" => [ "style" => "height: 48px; margin: 15px;"], "href" => "javascript: history.back();" ];
+    }
+
+    protected static function scriptForJsonLd(array $value): array {
+        return [ "tag" => "script", "attributes" => [ "type" => "application/ld+json"], "content" => (new StructuredData())->getJson($value) ];
     }
 }
