7b32d576e75539f6d5333f5d29b9806cb5b912b8
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2021-08-04 14:40:53 -0300

    Resource.php - added function to get host with schema

--- src/Element/Element.php
+++ src/Element/Element.php
@@ -1,89 +1,42 @@
 <?php
-
 namespace Plinct\Web\Element;
 
 class Element implements ElementInterface {
     protected $element = [];
 
-    public function __construct($tag = null, array $attributes = null, $content = null) {
-        if ($tag) {
-            $this->setElement($tag, $attributes, $content);
-        }
+    public function __construct($tag, array $attributes = null, $content = null) {
+        $this->element['tag'] = $tag;
+        if ($attributes) $this->element['attributes'] = $attributes;
+        if ($content) $this->element['content'] = $content;
     }
 
-    public function getElement(): array {
-        return $this->element;
-    }
-
-    protected function getLastElement(): int {
-        $count = count($this->element);
-        return $count == 0 ? 0 : $count - 1;
-    }
-
-    public function setElement(string $tagname, array $attributes = null, $content = null): ElementInterface {
-        $this->element[]['tag'] = $tagname;
-        if ($attributes) $this->addedInElement('attributes', $attributes);
-        if ($content) $this->addedInElement('content', $content);
-        return $this;
-    }
-
-    private function addedInElement(string $property, $value = null) {
-        $newValue = null;
-        $lastElement = $this->element[$this->getLastElement()][$property] ?? null;
-        if ($lastElement) {
-            if (is_string($lastElement)) {
-                $newValue = [$lastElement, $value];
-            } elseif (is_array($lastElement) && is_array($value)) {
-                $lastElement[] = $value;
-                $newValue = $lastElement;
-            }elseif (is_array($lastElement) && is_string($value)) {
-                $newValue = [ $lastElement, $value ];
-            }
-        } else {
-            if (is_array($value) && array_key_first($value) !== 0 && $property === 'content') {
-                $newValue = [ $value ];
-            } else {
-                $newValue = $value;
-            }
-        }
-        // ADDED
-        $this->element[$this->getLastElement()][$property] = $newValue;
-    }
-
-    public function attributes(array $attributes = null): ElementInterface {
-        $currentAttributes = $this->element[$this->getLastElement()]['attributes'] ?? null;
-        if ($attributes) {
-            if (isset($currentAttributes)) {
-                $this->element[$this->getLastElement()]['attributes'] = array_merge($currentAttributes,$attributes);
-            } else {
-                $this->addedInElement('attributes', $attributes);
-            }
-        }
+    protected function withObject(): Element {
+        $this->element['object'] = $this->element['tag'];
+        unset($this->element['tag']);
         return $this;
     }
 
     public function content($content): ElementInterface {
-        $this->addedInElement('content', $content);
+        $this->element['content'][] = $content;
+        return $this;
+    }
+
+    public function setAttribute($name,$value): ElementInterface {
+        $this->element['attributes'][$name] = $value;
+        return $this;
+    }
+
+    public function attributes(array $attributes): ElementInterface {
+        if (isset($this->element['attributes'])) {
+            $this->element['attributes'] = array_merge($this->element['attributes'],$attributes);
+        } else {
+            $this->element['attributes'] = $attributes;
+        }
         return $this;
     }
 
     public function href(string $href): ElementInterface {
-        $this->addedInElement('href', $href);
-        return $this;
-    }
-
-    public function div(array $attributes = null): ElementInterface {
-        $this->element[$this->getLastElement()]['content'][] = ['tag'=>'div','attributes'=>$attributes];
-        return $this;
-    }
-
-    public function p($content, array $attributes = null): ElementInterface {
-        $this->element[$this->getLastElement()]['content'][] = ['tag'=>'p','attributes'=>$attributes,'content'=>$content];
-        return $this;
-    }
-
-    public function h1($content, array $attributes = null): ElementInterface {
-        $this->element[$this->getLastElement()]['content'][] = ['tag'=>'h1','attributes'=>$attributes,'content'=>$content];
+        $this->element['href'] = $href;
         return $this;
     }
 
