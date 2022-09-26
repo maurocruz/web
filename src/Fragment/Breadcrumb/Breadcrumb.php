f5fffcb4249a55b43d00936d6aa6b16241f448ac
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2022-09-26 14:34:15 -0300

    fix error in breadcrumb if argument of the setBreadcrumb is null

--- src/Fragment/Breadcrumb/Breadcrumb.php
+++ src/Fragment/Breadcrumb/Breadcrumb.php
@@ -8,73 +8,72 @@ use Plinct\Web\Render;
 
 class Breadcrumb extends BreadcrumbAbstract implements BreadcrumbInterface
 {
-    /**
-     * @param array $attributes
-     * @return BreadcrumbInterface
-     */
-    public function attributes(array $attributes): BreadcrumbInterface
-    {
-        $this->setAttributes($attributes);
-        return $this;
+  /**
+   * @param array $attributes
+   * @return BreadcrumbInterface
+   */
+  public function attributes(array $attributes): BreadcrumbInterface
+  {
+    $this->setAttributes($attributes);
+    return $this;
+  }
+
+  /**
+   * @param string|null $breadcrumbSchema
+   * @return BreadcrumbInterface
+   */
+  public function setBreadcrumb(string $breadcrumbSchema = null): BreadcrumbInterface
+  {
+		if ($breadcrumbSchema) {
+			$withArray = json_decode($breadcrumbSchema, true);
+			$this->breadcrumb = isset($withArray['@type']) && $withArray['@type'] == 'BreadcrumbList' ? $withArray : null;
+		}
+    return $this;
+  }
+
+  /**
+   * @param string $url
+   * @return BreadcrumbInterface
+   */
+  public function setUrl(string $url): BreadcrumbInterface
+  {
+    if (!$this->breadcrumb) {
+      $breadcrumb = [
+        '@context'=>'https://schema.org',
+        '@type'=>'BreadcrumbList'
+      ];
+      $urlExplode = explode('/', $url);
+      $urlRoot = $urlExplode[0] . '//' . $urlExplode[2];
+      $href = $urlRoot;
+      $breadcrumb['itemListElement'][] = ['@type'=>'ListItem','position'=>1, 'item'=>['@id'=>$href,'name'=>_('Home')]];
+
+      for ($i = 3; $i < count($urlExplode); $i++) {
+        $href = $href . DIRECTORY_SEPARATOR . $urlExplode[$i];
+        $name = ucfirst($urlExplode[$i]);
+        $breadcrumb['itemListElement'][] = ['@type'=>'ListItem','position'=> ($i-1) , 'item'=>['@id'=>$href,'name'=>$name]];
+      }
+      $this->breadcrumb = $breadcrumb;
     }
+    return $this;
+  }
 
-    /**
-     * @param string|null $breadcrumbSchema
-     * @return BreadcrumbInterface
-     */
-    public function setBreadcrumb(string $breadcrumbSchema): BreadcrumbInterface
-    {
-        $withArray = json_decode($breadcrumbSchema, true);
-        $this->breadcrumb = isset($withArray['@type']) && $withArray['@type'] == 'BreadcrumbList' ? $withArray : null;
-        return $this;
-    }
+  /**
+   * @param array|null $baseBreadcrumb
+   * @param bool $byRequestUri
+   * @return string
+   */
+  final public function getNavbar(array $baseBreadcrumb = null, bool $byRequestUri = true): string
+  {
+    $this->setListWrapper();
+    $this->setListItems($baseBreadcrumb, $byRequestUri);
+    return Render::arrayToString($this->getListWrapper());
+  }
 
-    /**
-     * @param string $url
-     * @return BreadcrumbInterface
-     */
-    public function setUrl(string $url): BreadcrumbInterface
-    {
-        if (!$this->breadcrumb) {
-            $breadcrumb = [
-                '@context'=>'https://schema.org',
-                '@type'=>'BreadcrumbList'
-            ];
-
-            $urlExplode = explode('/', $url);
-            $urlRoot = $urlExplode[0] . '//' . $urlExplode[2];
-            $href = $urlRoot;
-            $breadcrumb['itemListElement'][] = ['@type'=>'ListItem','position'=>1, 'item'=>['@id'=>$href,'name'=>_('Home')]];
-
-            for ($i = 3; $i < count($urlExplode); $i++) {
-                $href = $href . DIRECTORY_SEPARATOR . $urlExplode[$i];
-                $name = ucfirst($urlExplode[$i]);
-                $breadcrumb['itemListElement'][] = ['@type'=>'ListItem','position'=> ($i-1) , 'item'=>['@id'=>$href,'name'=>$name]];
-            }
-            $this->breadcrumb = $breadcrumb;
-        }
-        return $this;
-    }
-
-    /**
-     * @param array|null $baseBreadcrumb
-     * @param bool $byRequestUri
-     * @return string
-     */
-    final public function getNavbar(array $baseBreadcrumb = null, bool $byRequestUri = true): string
-    {
-        $this->setListWrapper();
-
-        $this->setListItems($baseBreadcrumb, $byRequestUri);
-
-        return Render::arrayToString($this->getListWrapper());
-    }
-
-    /**
-     * @return string
-     */
-    final public function getBreadcrumb(): string
-    {
-        return json_encode($this->breadcrumb, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-    }
+  /**
+   * @return string
+   */
+  final public function getBreadcrumb(): string
+  {
+    return json_encode($this->breadcrumb, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+  }
 }
