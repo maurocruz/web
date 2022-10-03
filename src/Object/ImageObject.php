3fdd584091a9202b6a30ba23767c5052bef81868
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2022-10-03 14:49:57 -0300

    added source name in data attributes if image not exists

--- src/Object/ImageObject.php
+++ src/Object/ImageObject.php
@@ -34,7 +34,7 @@ class ImageObject
 		$src = $value['src'] ?? null;
 
 		// BROKEN IMAGE SVG
-    $returns = ['tag'=>'div','content'=>'<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="100%" height="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M19 5v11.17l2 2V5c0-1.1-.9-2-2-2H5.83l2 2H19zM2.81 2.81L1.39 4.22L3 5.83V19c0 1.1.9 2 2 2h13.17l1.61 1.61l1.41-1.41L2.81 2.81zM5 19V7.83l7.07 7.07l-.82 1.1L9 13l-3 4h8.17l2 2H5z" fill="#e3e3e3"/></svg>'];
+    $returns = ['tag'=>'div','attributes'=>['data-src'=>$src],'content'=>'<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="100%" height="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M19 5v11.17l2 2V5c0-1.1-.9-2-2-2H5.83l2 2H19zM2.81 2.81L1.39 4.22L3 5.83V19c0 1.1.9 2 2 2h13.17l1.61 1.61l1.41-1.41L2.81 2.81zM5 19V7.83l7.07 7.07l-.82 1.1L9 13l-3 4h8.17l2 2H5z" fill="#e3e3e3"/></svg>'];
 
     if ($src && $src !== '') {
       $pathinfo = pathinfo($src);
