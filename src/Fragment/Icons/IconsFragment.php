c6b6db73fa6c02c0d6f9f203c3df17b700217ec5
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2025-01-21 10:37:34 -0300

    added icons in fragments

--- src/Fragment/Icons/IconsFragment.php
+++ src/Fragment/Icons/IconsFragment.php
@@ -62,6 +62,19 @@ class IconsFragment
 	}
 
 	/**
+	 * @param string|null $uri
+	 * @param string $width
+	 * @param string $height
+	 * @param string $color
+	 * @return string
+	 */
+	public function email(string $uri = null, string $width = '1em', string $height = '1em', string $color = "#FFFFFF"): string
+	{
+		$icon = '<svg xmlns="http://www.w3.org/2000/svg" width="'.$width.'" height="'.$height.'" viewBox="0 0 24 24"><path fill="'.$color.'" d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2m0 4l-8 5l-8-5V6l8 5l8-5z"/></svg>';
+		return $uri ? "<a href='{$uri}' target='_blank'>{$icon}</a>" : $icon;
+	}
+
+	/**
 	 * @param int $width
 	 * @param int $height
 	 * @return string
