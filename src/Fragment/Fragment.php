f59d66c52b2213fae6691e91bd026300108ebe0f
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2025-06-29 10:04:21 -0300

    Added sitemap icon in fragments

--- src/Fragment/Fragment.php
+++ src/Fragment/Fragment.php
@@ -1,5 +1,4 @@
 <?php
-declare(strict_types=1);
 namespace Plinct\Web\Fragment;
 
 use Plinct\Web\Element\Figure;
--- src/Fragment/Icons/IconsFragment.php
+++ src/Fragment/Icons/IconsFragment.php
@@ -124,6 +124,11 @@ class IconsFragment
     return '<svg xmlns="http://www.w3.org/2000/svg" width="'.$width.'" height="'.$height.'" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M12 20v-8m0 0V4m0 8h8m-8 0H4"/></svg>';
   }
 
+	public function sitemap(int $width = 16, int $height = 16): string
+	{
+		return '<svg xmlns="http://www.w3.org/2000/svg"  width="'.$width.'" height="'.$height.'" viewBox="0 0 1792 1536"><path fill="currentColor" d="M1792 1120v320q0 40-28 68t-68 28h-320q-40 0-68-28t-28-68v-320q0-40 28-68t68-28h96V832H960v192h96q40 0 68 28t28 68v320q0 40-28 68t-68 28H736q-40 0-68-28t-28-68v-320q0-40 28-68t68-28h96V832H320v192h96q40 0 68 28t28 68v320q0 40-28 68t-68 28H96q-40 0-68-28t-28-68v-320q0-40 28-68t68-28h96V832q0-52 38-90t90-38h512V512h-96q-40 0-68-28t-28-68V96q0-40 28-68t68-28h320q40 0 68 28t28 68v320q0 40-28 68t-68 28h-96v192h512q52 0 90 38t38 90v192h96q40 0 68 28t28 68"/></svg>';
+	}
+
 	/**
 	 * @param int $width
 	 * @param int $height
