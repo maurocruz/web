0ac54b91af6c2de4a079b8817194bbb4d6083f05
Mauro Cruz <maurocruz@pirenopolis.tur.br>
2025-11-12 11:31:22 -0300

    - Eliminei height=100%  no estilo do svg broken images

--- src/Object/ImageObject.php
+++ src/Object/ImageObject.php
@@ -1,7 +1,4 @@
 <?php
-
-declare(strict_types=1);
-
 namespace Plinct\Web\Object;
 
 use Exception;
@@ -32,25 +29,19 @@ class ImageObject
   public function __invoke($value): ?array
   {
 		$src = $value['src'] ?? null;
-
 		// BROKEN IMAGE SVG
-    $returns = ['tag'=>'div','attributes'=>['data-src'=>$src],'content'=>'<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="100%" height="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M19 5v11.17l2 2V5c0-1.1-.9-2-2-2H5.83l2 2H19zM2.81 2.81L1.39 4.22L3 5.83V19c0 1.1.9 2 2 2h13.17l1.61 1.61l1.41-1.41L2.81 2.81zM5 19V7.83l7.07 7.07l-.82 1.1L9 13l-3 4h8.17l2 2H5z" fill="#e3e3e3"/></svg>'];
-
+    $returns = ['tag'=>'div','attributes'=>['data-src'=>$src],'content'=>'<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="100%" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M19 5v11.17l2 2V5c0-1.1-.9-2-2-2H5.83l2 2H19zM2.81 2.81L1.39 4.22L3 5.83V19c0 1.1.9 2 2 2h13.17l1.61 1.61l1.41-1.41L2.81 2.81zM5 19V7.83l7.07 7.07l-.82 1.1L9 13l-3 4h8.17l2 2H5z" fill="#e3e3e3"/></svg>'];
     if ($src && $src !== '') {
       $pathinfo = pathinfo($src);
       $width = isset($value['width']) && $value['width'] != '0' ? $value['width'] : null;
       $height = isset($value['height']) && $value['height'] != '0' ? $value['height'] : null;
-
       // SVG TYPE
       if (isset($pathinfo['extension']) && $pathinfo['extension'] == "svg") {
           $returns = [ "tag" => "img", "attributes" => [ 'src' => $src ]];
-
       } else {
 	      // CALL IMAGE CLASS
 	      $image = new Image($src);
-
 	      if (!$image->isValidImage()) return $returns;
-
         // SET VARS
         $caption = isset($value['caption']) ? strip_tags($value['caption']) : null;
         $alt = $value['alt'] ?? $caption ?? "Image: " . basename($src);
@@ -59,46 +50,36 @@ class ImageObject
         $this->srcsetAttributes['sizes'] = "";
         $this->srcsetAttributes['srcset'] = "";
         $this->srcsetAttributes['src'] = "";
-
-
         // THUMBNAIL
 	      if ($width) {
           // CREATE THUMBNAIL
           $src = $image->thumbnail($width, $height);
-
           // CREATE OTHER SRCSET IMAGES
           if ($width > self::SRCSET_SMALL) {
               $this->srcsetAttributes($image, self::SRCSET_SMALL);
           }
-
           if ($width > self::SRCSET_MEDIUM) {
               $this->srcsetAttributes($image, self::SRCSET_MEDIUM);
           }
-
           if ($width > self::SRCSET_LARGE) {
               $this->srcsetAttributes($image, self::SRCSET_LARGE);
           }
-
           // SET ATTRIBUTES
           $this->srcsetAttributes['sizes'] .= " {$image->getNewWidth()}px";
           $this->srcsetAttributes['srcset'] .= sprintf("%s %sw", $src, $image->getNewWidth());
           $this->srcsetAttributes['src'] .= $image->getSrc();
-
         } else {
           $this->srcsetAttributes = ["src" => $image->getSrc()];
         }
-
         // APPEND IN IMG ELEMENT
         $returns = ["tag" => "img", "attributes" => array_merge($attributes, $this->srcsetAttributes)];
       }
-
       // IF HREF
       if (isset($value['href']) && $value['href'] !== '') {
           $hrefAttr = isset($value['hrefAttributes']) ? array_merge(["href" => $value['href']], $value['hrefAttributes']) : ["href" => $value['href']];
           $returns = ["tag" => "a", "attributes" => $hrefAttr, "content" => $returns];
       }
     }
-
     // RESPONSE
     return $returns;
   }
@@ -107,7 +88,7 @@ class ImageObject
    * @param $image
    * @param $newwidth
    */
-  private function srcsetAttributes($image, $newwidth)
+  private function srcsetAttributes($image, $newwidth): void
   {
     $height = (string) ($newwidth / $image->getNewRatio());
     $thumb =  $image->thumbnail($newwidth, $height);
